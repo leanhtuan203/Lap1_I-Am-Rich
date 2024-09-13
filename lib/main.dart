ade0f52a5a27793f1a93dc101f79789dd5390f67
ducshntrong <huynhtrongduc01225445811@gmail.com>
2024-09-13 21:09:27 +0700

    lab4

--- lib/main.dart
+++ lib/main.dart
@@ -1,80 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:audioplayers/audioplayers.dart';
+import 'dart:math';
+void main() => runApp(
+  MaterialApp(
+    home: BallPage(),
+  ),
+);
 
-void main() {
-  return runApp(const XylophoneApp());
-}
-
-// class XylophoneApp extends StatelessWidget {
-//   const XylophoneApp({super.key});
-//
-//   void playSound(int soundNumber){
-//     final player = AudioPlayer();
-//     player.play(AssetSource('note$soundNumber.wav'));
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         body: SafeArea(
-//           child: Column(
-//             children: [
-//               TextButton(
-//                 onPressed: () {},
-//                 child: child),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-class XylophoneApp extends StatelessWidget {
-  const XylophoneApp({super.key});
-
-  void playSound(int soundNumber){
-    final player = AudioPlayer();
-    player.play(AssetSource('note$soundNumber.wav'));
-  }
-
-  Expanded buildKey(Color color, int soundNumber){
-    return Expanded(
-        flex: 1,
-        child: ElevatedButton(
-            onPressed: () {playSound(soundNumber);},
-            style: ElevatedButton.styleFrom(
-              backgroundColor: color,
-              shape: const RoundedRectangleBorder(
-                borderRadius: BorderRadius.zero, // Đặt góc bo bằng 0
-              ),
-            ),
-            child: null)
-    );
-  }
-
+class BallPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.black,
-        body: SafeArea(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              buildKey(Colors.red, 1),
-              buildKey(Colors.orange, 2),
-              buildKey(Colors.yellow, 3),
-              buildKey(Colors.green, 4),
-              buildKey(Colors.teal, 5),
-              buildKey(Colors.blue, 6),
-              buildKey(Colors.purple, 7),
-            ],
-          )),
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      appBar: AppBar(
+        backgroundColor: Colors.blue.shade900,
+        title: Text('Ask Me Anything'),
       ),
+      body: Ball(),
     );
   }
 }
 
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
 
+class _BallState extends State<Ball> {
+
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: TextButton(
+          onPressed: (){
+            setState(() {
+              ballNumber = Random().nextInt(5) + 1;
+            });
+          },
+          child: Image.asset('images/ball$ballNumber.png')),
+    );
+  }
+}
